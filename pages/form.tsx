6d6194c95944c787dee93d66fd7dfa8edd9c4d56
Ashay Parikh <ashayparikh22@gmail.com>
2023-02-17 17:41:58 -0600

    Read input from form

--- pages/form.tsx
+++ pages/form.tsx
@@ -15,9 +15,15 @@ const Form = () => {
             e.preventDefault();
             try {
               const data = new FormData(e.currentTarget);
-              console.log(data);
+              
+              const solarPanelDirection = data.get('solarPanelDirection') as string;
+              const solarPanelAngelTilt = data.get('solarPanelAngelTilt') as string;
+              const solarPanelCapacity = data.get('solarPanelCapacity') as string;
+
+              console.log(`Direction: ${solarPanelDirection}, Angel Tilt: ${solarPanelAngelTilt}, Capacity: ${solarPanelCapacity}`);
+
             } catch (e) {
-              console.log(e);
+              console.log(`onSubmit(): ${e}`);
             }
           }}
         >
