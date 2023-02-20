aa5bb522551b7da5675e036c0545e1a4cef2645f
Ashay Parikh <ashayparikh22@gmail.com>
2023-02-20 14:17:49 -0600

    Add form validation, clean up styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { Inter } from '@next/font/google';
 
@@ -7,8 +8,18 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>Open Climate Fix</title>
+        <meta name="description" content="pv-sites-mobile" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`${inter.className} bg-white dark:bg-black flex flex-col items-center justify-start px-10 min-h-screen"`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
