b9e36374a1dda2cae386a2f37040586210a49b8e
Jacky Park <jackypark9852@gmail.com>
2023-02-20 19:31:32 -0600

    Add page title

--- pages/test/location_input.tsx
+++ pages/test/location_input.tsx
@@ -3,7 +3,8 @@ import MapBoxInput from "~/components/mapbox_input";
 export default function Home() {
   return (
     <div className="h-screen p-4" id = "rootDiv">
-      <div className="flex h-fit " id = "flexBox">
+      <div className="flex flex-col h-fit gap-1" id = "flexBox">
+        <h1 className="text-xl">Mapbox Input Test</h1>
         <div className="w-full h-96" id = "mapboxInputWrapper">
           <MapBoxInput/>
         </div>
