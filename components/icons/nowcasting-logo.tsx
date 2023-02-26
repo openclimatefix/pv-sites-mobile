4d8254a46eb90df52738320341abddbd3dec0fbc
ericcccliu <liueric360@gmail.com>
2023-02-26 12:43:36 -0600

    make prettier happy

--- components/icons/nowcasting-logo.tsx
+++ components/icons/nowcasting-logo.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 
 type NowcastingLogoProps = {
-    width: string; 
-    height: string;
-}
+  width: string;
+  height: string;
+};
 
-export const NowcastingLogo: React.FC<NowcastingLogoProps> = ({width, height}) => (
+export const NowcastingLogo: React.FC<NowcastingLogoProps> = ({
+  width,
+  height,
+}) => (
   <a href="https://www.openclimatefix.org/" target="_blank" rel="noreferrer">
     <Image
       src="/nowcasting-secondary.svg"
--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,11 +1,11 @@
-import { handleLogout } from '@auth0/nextjs-auth0'
-import { NextApiHandler } from 'next'
+import { handleLogout } from '@auth0/nextjs-auth0';
+import { NextApiHandler } from 'next';
 
 const handler: NextApiHandler = (req, res) => {
-  const { returnTo } = req.query
+  const { returnTo } = req.query;
   return handleLogout(req, res, {
-    returnTo: Array.isArray(returnTo) ? returnTo[0] : returnTo
-  })
-}
+    returnTo: Array.isArray(returnTo) ? returnTo[0] : returnTo,
+  });
+};
 
-export default handler
+export default handler;
