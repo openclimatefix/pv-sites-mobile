63a8d281a27f7a262585fe4b12dff9d70452b2aa
Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
2023-02-27 15:46:00 -0600

    Add NavBar and SideBar Components (#40)
    * Add navbar and sidebar outline components
    
    * Adds sidebar nav links and overlay
    
    * Modularize buttons and fix svg imports
    
    * Fix PR comments and add NavBar to layout
    
    * Address final PR comments
    
    * Resolve PR comments

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
+import Sidebar from './SideBar';
+import NavBar from './NavBar';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <main className="bg-white dark:bg-black flex flex-col items-center justify-start px-10 min-h-screen">
+      <NavBar />
+      <Sidebar />
       {children}
     </main>
   );
