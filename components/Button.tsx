9cdfdd1c854d1c189b9ee53529ee31c459339006
Jacky Park <jackypark9852@gmail.com>
2023-02-27 22:00:38 -0600

    Update button styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ const Button: FC<PropsWithChildren<Props>> = ({ children, enabled }) => {
   return (
     <button
       disabled={!enabled}
-      className="bg-ocf-gray dark:bg-ocf-gray shadow h-full w-full text-center rounded-md font-bold text-xl uppercase"
+      className="bg-ocf-yellow disabled:bg-ocf-gray disabled:dark:bg-ocf-gray shadow h-full w-full text-center rounded-md font-bold text-xl uppercase"
     >
       {children}
     </button>
