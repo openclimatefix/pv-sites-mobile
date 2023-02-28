7174bc86d8575157ec4b20d6ad7f2d7af22387b8
ericcccliu <liueric360@gmail.com>
2023-02-27 18:59:02 -0600

    fix format errors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,8 +5,6 @@ import NavBar from './NavBar';
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <main className="bg-white dark:bg-black flex flex-col items-center justify-start px-10 min-h-screen">
-      <NavBar />
-      <Sidebar />
       {children}
     </main>
   );
--- pages/logout.tsx
+++ pages/logout.tsx
@@ -28,9 +28,7 @@ const Logout = () => {
       </div>
       <div className="flex flex-col items-center justify-end h-1/2 w-full pb-28">
         <Link href="/">
-          <button
-            className="bg-ocf-yellow dark:bg-ocf-yellow shadow h-14 w-72 text-center rounded-md font-bold text-xl"
-          >
+          <button className="bg-ocf-yellow dark:bg-ocf-yellow shadow h-14 w-72 text-center rounded-md font-bold text-xl">
             Back to login
           </button>
         </Link>
