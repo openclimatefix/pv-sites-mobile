3fc82b54e3cbc5ab0a1981a47569a88542c29e81
neha-vard <neha.vard@gmail.com>
2023-02-27 22:15:34 -0600

    Retirieve and display current output and percent yield for specific time

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,8 +2,25 @@ import NumberDisplay from '../components/NumberDisplay';
 import Warnings from '../components/Warnings';
 import Graph from '../components/Graph';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import useSWR from 'swr';
 
 const Dashboard = () => {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data: pv_actual } = useSWR(
+    '/api/sites/pv_actual/b97f68cd-50e0-49bb-a850-108d4a9f7b7e',
+    fetcher
+  );
+  let cur_time = '2023-02-15T23:30:00+00:00';
+  let cur_output = pv_actual?.pv_actual_values.filter(
+    (item: { datetime_utc: string; actual_generation_kw: number }) => {
+      return item.datetime_utc == cur_time;
+    }
+  )[0].actual_generation_kw;
+
+  const { data: site_list } = useSWR('/api/sites/site_list', fetcher);
+  console.log(site_list.site_list[0].installed_capacity_kw);
+  let installed_capacity_kw = site_list?.site_list[0].installed_capacity_kw;
+
   return (
     <div className="bg-black w-screen h-screen px-4">
       <h1 className="mt-24 text-ocf-gray text-3xl font-bold">Dashboard</h1>
@@ -14,8 +31,11 @@ const Dashboard = () => {
         <NumberDisplay title="Today's Expected Output" value="2700kW" />
       </div>
       <div className="flex flex-row w-full justify-center space-x-4">
-        <NumberDisplay title="Current Output" value="2200kW" />
-        <NumberDisplay title="Current Capacity" value="80%" />
+        <NumberDisplay title="Current Output" value={`${cur_output}kW`} />
+        <NumberDisplay
+          title="Current Capacity"
+          value={`${cur_output / installed_capacity_kw}%`}
+        />
       </div>
       <div className="flex flex-row w-full justify-start">
         <Graph />
