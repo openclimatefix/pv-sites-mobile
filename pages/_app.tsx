9519dd8cdaaf3d55cbb18bce09227656cbff9469
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-03-01 17:01:25 -0600

    Downgrade next.js/auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { UserProvider } from '@auth0/nextjs-auth0/';
 import Head from 'next/head';
 
 import { AppProvider } from '~/components/context';
