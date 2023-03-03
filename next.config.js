4b75acc48797ef81ddb409f3f438169d8670f48d
code-arman <armanr123@gmail.com>
2023-03-03 00:50:43 -0600

    Init PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: false
+});
 
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
 module.exports = nextConfig;
---
+++ pages/_offline.tsx
@@ -0,0 +1,11 @@
+export default function Offline() {
+  return (
+    <div>
+      <main>
+        <h1>
+          <a>Oops! We are Offline right now!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
