61ad6674acf2320a5070c834793b3d56179153f6
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-03-02 20:27:38 -0600

    Style icons to be yellow when user is on that specific page

--- components/BottomNavBar.tsx
+++ components/BottomNavBar.tsx
@@ -2,6 +2,16 @@ import Link from 'next/link';
 import DashboardIcon from './icons/DashboardIcon';
 import SearchIcon from './icons/SearchIcon';
 import SiteListIcon from './icons/SiteListIcon';
+import { useRouter } from 'next/router';
+import useSWR, { Fetcher } from 'swr';
+
+interface SiteListProps {
+  site_list: SiteProps[];
+}
+
+const fetcher: Fetcher<SiteListProps> = async (url: string) => {
+  return await fetch(url).then((res) => res.json());
+};
 
 const icons = [
   {
@@ -22,13 +32,23 @@ const icons = [
 ];
 
 const BottomNavBar = () => {
+  const { asPath } = useRouter();
+  const { data, isLoading } = useSWR(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/sites/site_list`,
+    fetcher
+  );
+
   return (
     <div className="bg-ocf-gray-1000 w-screen h-[80px] bottom-0 fixed mt-px">
       <div className="flex justify-evenly mt-[15px]">
         {icons.map((val, i) => {
           return (
             <Link key={i} href={val.link}>
-              <a className="text-white text-xs items-center mt-[10px] flex flex-col justify-evenly">
+              <a
+                className={`text-xs items-center mt-[10px] flex flex-col justify-evenly ${
+                  asPath == val.link ? 'text-ocf-yellow' : 'text-white'
+                }`}
+              >
                 <val.icon key={i} color="white"></val.icon>
                 <p className="mt-[5px]">{val.title}</p>
               </a>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
     <>
       <NavBar />
       <Sidebar />
-      <main className="bg-white dark:bg-ocf-black flex flex-col items-center justify-start px-10">
+      <main className="bg-white dark:bg-ocf-black flex flex-col items-center justify-start px-10 mb-[50px]">
         {children}
         <BottomNavBar></BottomNavBar>
       </main>
