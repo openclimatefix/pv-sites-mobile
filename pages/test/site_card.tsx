bcb89576bb0e4085764ed6178238110af7ad1d11
Jacky Park <jackypark9852@gmail.com>
2023-03-02 19:11:37 -0600

    Create test site

---
+++ pages/test/site_card.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-full flex flex-col bg-white">
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
