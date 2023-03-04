13e84ea965e84e67069359bd1c801055de265e35
Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
2023-03-04 13:32:34 -0600

    Implement form context provider and post data (#57)
    * Add basic form context and provider
    
    * Fix form context and provider
    
    * Add form context functionality to form page
    
    * Add form context to location page
    
    * Fix post data from inputs
    
    * Address PR comments
    
    * Fix imports
    
    * Address PR comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,24 +3,30 @@ import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/';
 import Head from 'next/head';
 
-import { AppProvider } from '~/components/context';
+import { SidebarProvider } from '~/lib/context/sidebar_context';
+import { FormProvider } from '~/lib/context/form_context';
 
 import Layout from '~/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <AppProvider>
-        <Head>
-          <title>Open Climate Fix</title>
-          <meta name="description" content="pv-sites-mobile" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AppProvider>
+      <FormProvider>
+        <SidebarProvider>
+          <Head>
+            <title>Open Climate Fix</title>
+            <meta name="description" content="pv-sites-mobile" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SidebarProvider>
+      </FormProvider>
     </UserProvider>
   );
 }
