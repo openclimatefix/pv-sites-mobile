fccf3c149644ad7bc7bd5684b89ba2376c3ac9e9
Ashay Parikh <ashayparikh22@gmail.com>
2023-03-05 11:36:36 -0600

    Create Near Future Threshold Graph (#56)
    * create future graph component
    
    * add most of future graph
    
    * add labels
    
    * finish graph
    
    * moved helper functions to utils
    
    ---------
    
    Co-authored-by: ericcccliu <liueric360@gmail.com>

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,16 @@
+import ThresholdGraph from '~/components/ThresholdGraph';
 import NumberDisplay from '../components/NumberDisplay';
-import Warnings from '../components/Warnings';
 import Graph from '../components/Graph';
 import CurrentOutput from '../components/CurrentOutput';
 import CurrentCapacity from '../components/CurrentCapacity';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import useSWR from 'swr';
 
 const Dashboard = () => {
   return (
     <div className="bg-ocf-black w-screen h-screen px-4">
       <h1 className="mt-4 text-ocf-gray text-3xl font-bold">Dashboard</h1>
       <div className="flex flex-row w-full justify-start">
-        <Warnings />
+        <ThresholdGraph />
       </div>
       <div className="flex flex-row w-full justify-start">
         <NumberDisplay title="Today's Expected Output" value="2700 kW" />
