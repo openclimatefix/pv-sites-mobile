8ccb5c6adeb3dc270645cd4634ec31fbc317230a
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-03-06 21:45:21 -0600

    Remove extraneous declarations of the SitListProps after it was moved to types.ts

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,27 +2,7 @@ import { FC } from 'react';
 import { useSidebarContext } from './context';
 import { NowcastingLogo, MenuLogo } from './icons/navbar_icons';
 import useSWR, { Fetcher } from 'swr';
-
-interface SiteProps {
-  site_uuid: string;
-  client_name: string;
-  client_site_id: string;
-  client_site_name: string;
-  region?: string;
-  dno?: string;
-  gsp?: string;
-  orientation?: number;
-  tilt?: number;
-  latitude: number;
-  longitude: number;
-  installed_capacity_kw: number;
-  created_utc: string;
-  updated_utc: string;
-}
-
-interface SiteListProps {
-  site_list: SiteProps[];
-}
+import { SiteListProps } from '~/lib/types';
 
 const fetcher: Fetcher<SiteListProps> = async (url: string) => {
   return await fetch(url).then((res) => res.json());
