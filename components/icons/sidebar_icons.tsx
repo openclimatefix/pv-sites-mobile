c3274ea961b41b94dccf3f8df5fcced31b08b2b5
Jacky Park <jackypark9852@gmail.com>
2023-03-06 21:56:35 -0600

    Add edit icon

--- components/icons/sidebar_icons.tsx
+++ components/icons/sidebar_icons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 export const LocationIcon: React.FC = ({}) => (
   <svg
@@ -17,14 +17,19 @@ export const LocationIcon: React.FC = ({}) => (
   </svg>
 );
 
-export const EditIcon: React.FC = ({}) => (
+interface EditIconProps {
+  className?: string;
+}
+export const EditIcon: React.FC<PropsWithChildren<EditIconProps>> = ({
+  className = 'w-6 h-6',
+}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="white"
-    className="w-6 h-6"
+    className={className}
   >
     <path
       strokeLinecap="round"
--- pages/test/site_card.tsx
+++ pages/test/site_card.tsx
@@ -1,8 +1,13 @@
 import SiteCard from '~/components/SiteCard';
+import { EditIcon } from '~/components/icons/sidebar_icons';
 
 export default function Home() {
   return (
     <div className="h-full w-full flex flex-col gap-3 items-center px-5">
+      <div className="flex flex-row w-full h-12 items-end mb-4 pr-3">
+        <h1 className="flex-1 font-bold text-3xl text-ocf-gray">My Sites</h1>
+        <EditIcon className=" h-7" />
+      </div>
       <SiteCard isEditMode={true} />
       <SiteCard isEditMode={false} />
       <SiteCard isEditMode={false} />
