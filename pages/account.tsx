6b3110fb66c59f9751516b81b298bc465d5e1f95
AndrewLester <alester220@gmail.com>
2023-03-06 23:28:24 -0600

    Fix preview deployments + build

--- pages/account.tsx
+++ pages/account.tsx
@@ -44,7 +44,7 @@ const Account: NextPage<Props> = ({ inverters }) => {
           Linking status success: {query.linkSuccess}
         </p>
       )}
-      {inverters.length > 0 ? (
+      {inverters?.length > 0 ? (
         <AccountInfo inverters={inverters} />
       ) : (
         <Link href="/api/enode/link">
