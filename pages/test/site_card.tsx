912a66c22a956d4f36d7e741b5434542dcdf470f
Jacky Park <jackypark9852@gmail.com>
2023-03-06 21:09:50 -0600

    Remove padding in the wrapper

--- pages/test/site_card.tsx
+++ pages/test/site_card.tsx
@@ -2,7 +2,7 @@ import SiteCard from '~/components/SiteCard';
 
 export default function Home() {
   return (
-    <div className="h-full w-full flex flex-col gap-3 items-center">
+    <div className="h-full w-full flex flex-col gap-3 items-center px-5">
       <SiteCard />
       <SiteCard />
       <SiteCard />
