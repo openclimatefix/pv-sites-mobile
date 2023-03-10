ff1d56eef78667369fdf9ea8d5acce6df037ed01
anyaparekh <anzparekh@gmail.com>
2023-03-09 20:05:10 -0600

    Add power info code component

--- components/PowerInfoCard.tsx
+++ components/PowerInfoCard.tsx
@@ -1,10 +1,32 @@
 import { FC } from 'react';
-import * as data from './power_card_data.json';
+import Image from 'next/image';
 
-const PowerInfoCard: FC = () => {
+type PowerInfo = {
+  src: string;
+  applicance: string;
+  kWh: string;
+};
+
+const PowerInfoCard: FC<PowerInfo> = ({ src, applicance, kWh }) => {
   return (
-    <div className="bg-ocf-gray-1000 br-8">
-      <p>hello</p>
+    <div className="w-full">
+      <div className="bg-ocf-gray-1000 pt-3 pb-7 rounded-lg flex justify-center align-middle">
+        <Image
+          src={src}
+          alt={applicance}
+          width={75}
+          height={75}
+          className="flex-1"
+        />
+        <div className="bg-ocf-gray-1000 rounded-2xl ml-3 self-center">
+          <p className="text-ocf-gray text-left m-0 text-base mb-1 font-semibold">
+            {applicance}
+          </p>
+          <p className="text-ocf-gray text-left m-0 text-sm font-semibold">
+            {kWh} kWh
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
