0800e51b0faf2ae2f6d109bada350821c5f2b30b
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-03-11 14:30:13 -0600

    remove comments and add function description

--- lib/hooks/useTime.ts
+++ lib/hooks/useTime.ts
@@ -3,16 +3,20 @@ import { formatter } from '../utils';
 
 const SunCalc = require('suncalc');
 
+/**
+ * Creates a hook more accessesing specific values like the current time, whether it is daytime
+ * @param latitude, the latitude float value that is passed in
+ * @param longitude, the longitude number value that is passed in
+ * @returns currentTimeNoFilter, unfiltered version of the current time (right now) that a user can format
+ * @returns isDaytime, boolean value indicating whether it is daytime or not based on current time zone.
+ */
 const useTime = (latitude: number, longitude: number) => {
   const [currentTimeNoFilter, setCurrentTimeNoFilter] = useState(Date.now());
 
-  // get sunrise/sunset info for london
+  // get sunrise/sunset time fors passed in location
   const times = SunCalc.getTimes(Date.now(), latitude, longitude);
   const sunriseTime = times.sunrise;
   const sunsetTime = times.sunset;
-  console.log(sunriseTime);
-  console.log(currentTimeNoFilter);
-  console.log(sunsetTime);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
