1e22187296b2adf9a03bb58311c30366318e9ac2
Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
2023-03-13 18:58:02 -0500

    Add power infographic card carousel (#84)
    * Add power infographic card data
    
    * Add power info code component
    
    * Create more info page with carousel component
    
    * Remove unecessary file
    
    * resolve withPageAuthRequired import and rename moreinfo.tsx => more-info.tsx
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update data/power_card_data.json
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update data/power_card_data.json
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Eric Liu <62641231+ericcccsliu@users.noreply.github.com>
    
    * Address PR comments
    
    * Address carousel styling PR comments
    
    ---------
    
    Co-authored-by: Ashay Parikh <ashayparikh22@gmail.com>
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    Co-authored-by: Eric Liu <62641231+ericcccsliu@users.noreply.github.com>

---
+++ components/PowerInfoCard.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import Image from 'next/image';
+
+type PowerInfo = {
+  src: string;
+  appliance: string;
+  kW: string;
+};
+
+const PowerInfoCard: FC<PowerInfo> = ({ src, appliance, kW }) => {
+  return (
+    <div className="w-full pt-3 pb-8 flex justify-center align-middle gap-4">
+      <Image
+        src={src}
+        alt={appliance}
+        width={75}
+        height={75}
+        className="flex-1"
+      />
+      <div className="text-ocf-gray ml-3 self-center font-semibold text-left">
+        <p className="m-0 text-base mb-1 font-semibold">{appliance}</p>
+        <p className="text-sm ">{kW} kW</p>
+      </div>
+    </div>
+  );
+};
+
+export default PowerInfoCard;
---
+++ pages/more-info.tsx
@@ -0,0 +1,79 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import content from '../content/power-card-content.json';
+import { Carousel } from 'react-responsive-carousel';
+import PowerInfoCard from '~/components/PowerInfoCard';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Image from 'next/image';
+
+const MoreInfo = () => {
+  return (
+    <div className="bg-ocf-black w-screen min-h-screen px-4">
+      <h1 className="my-2 mt-4 text-ocf-gray text-3xl font-bold">More Info</h1>
+      <div className="bg-ocf-gray-1000 rounded-lg">
+        <Carousel
+          showStatus={false}
+          showThumbs={false}
+          renderArrowPrev={(clickHandler) => {
+            return (
+              <div
+                className={`absolute top-0 bottom-0 left-0 flex items-center p-3 cursor-pointer z-20`}
+                onClick={clickHandler}
+              >
+                <Image
+                  src="/left-arrow.svg"
+                  alt="left-arrow"
+                  height={16}
+                  width={10}
+                />
+              </div>
+            );
+          }}
+          renderArrowNext={(clickHandler) => {
+            return (
+              <div
+                className="absolute top-0 bottom-0 right-0 flex items-center p-3 cursor-pointer z-20"
+                onClick={clickHandler}
+              >
+                <Image
+                  src="/right-arrow.svg"
+                  alt="left-arrow"
+                  height={16}
+                  width={10}
+                />
+              </div>
+            );
+          }}
+          renderIndicator={(clickHandler, isSelected, index, label) => {
+            return (
+              <li
+                className={`rounded-full w-[8px] h-[8px] cursor-pointer mx-2 inline-block ${
+                  isSelected ? 'bg-ocf-yellow' : 'bg-ocf-gray-200'
+                }`}
+                onClick={clickHandler}
+                onKeyDown={clickHandler}
+                value={index}
+                key={index}
+                role="button"
+                tabIndex={0}
+                aria-label={`${label} ${index + 1}`}
+              />
+            );
+          }}
+        >
+          {content.appliances.map((element) => (
+            <PowerInfoCard
+              key={element.name}
+              src={element.icon}
+              appliance={element.name}
+              kW={element.kW}
+            />
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default MoreInfo;
+
+export const getServerSideProps = withPageAuthRequired();
