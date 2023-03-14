4ee538195a313dfdfb273110da23957f4826c25d
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-03-14 01:18:19 -0500

    Implement power requirement dashboard module (#106)
    * Add power infographic card data
    
    * Add power info code component
    
    * Create more info page with carousel component
    
    * Remove unecessary file
    
    * resolve withPageAuthRequired import and rename moreinfo.tsx => more-info.tsx
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update data/power_card_data.json
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update data/power_card_data.json
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    
    * Update components/PowerInfoCard.tsx
    
    Co-authored-by: Eric Liu <62641231+ericcccsliu@users.noreply.github.com>
    
    * Address PR comments
    
    * Initialize dashboard component to display info
    
    * Create json file for recs and add component to dashboard
    
    * Implement method to find the applicance with the greatest kw req less than or equal to the currentOutput
    
    * Delete different file for json content and add extra info to power-car-content.json and alter code to conditionally render if currentOuput is undefined
    
    * Remove comments
    
    * remove classname from outer div
    
    * Switch for loop with for each loop to reduce clutter
    
    * Fix spelling errors and add ternary operator for recommendationIdx
    
    * Address pr comments
    
    * Centered content in the EnergyRec component
    
    * Address final PR concerns and remove comments
    
    ---------
    
    Co-authored-by: anyaparekh <anzparekh@gmail.com>
    Co-authored-by: Ashay Parikh <ashayparikh22@gmail.com>
    Co-authored-by: Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
    Co-authored-by: Andrew Lester <alester220@gmail.com>
    Co-authored-by: Eric Liu <62641231+ericcccsliu@users.noreply.github.com>

---
+++ components/dashboard-modules/EnergyRecommendation.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+import { FC } from 'react';
+import { useSiteData } from '~/lib/hooks';
+import { getCurrentTimeForecast } from '~/lib/utils';
+import content from '../../content/power-card-content.json';
+import NumberDisplay from './NumberDisplay';
+
+/**
+ * Determines the appliance with the greatest energy required that is less than or equal to the current output
+ * @param currentOutput the current output in kW based on the getCurrentTimeForecast
+ * @returns the index of the appliance with the greatest enrgy requiement less than or equal to the current ouput
+ * or -1 if an index could not be found
+ */
+
+const getBestRecommendationIndex = (currentOutput: number) => {
+  let maxIndex = null;
+  let maxKW = 0;
+
+  content.appliances.forEach((appliance, i) => {
+    if (Number(appliance.kW) > maxKW && Number(appliance.kW) <= currentOutput) {
+      maxIndex = i;
+      maxKW = Number(appliance.kW);
+    }
+  });
+
+  return maxIndex;
+};
+
+const EnergyRecommendation: FC<{ siteUUID: string }> = ({ siteUUID }) => {
+  const { forecastData, isLoading } = useSiteData(siteUUID);
+  const currentOutput = forecastData
+    ? getCurrentTimeForecast(forecastData.forecast_values)
+    : undefined;
+
+  const recommendationIdx = currentOutput
+    ? getBestRecommendationIndex(currentOutput)
+    : null;
+
+  if (recommendationIdx === null) {
+    return <NumberDisplay title="Recommendations" value="N/A" />;
+  } else {
+    const appliance = content.appliances[recommendationIdx];
+    return (
+      <div
+        className="
+          flex-1
+          flex
+          my-2
+          p-4
+          text-center
+          justify-center
+          align-center
+          bg-ocf-gray-1000
+          rounded-2xl"
+      >
+        <Image
+          src={appliance.icon}
+          alt={appliance.name}
+          width={34.5}
+          height={28.75}
+        />
+        <div className="text-ocf-gray ml-3 self-center font-normal text-left flex-1 max-w-max">
+          <p className="m-0 text-[10px] mb-1">{appliance.description}</p>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default EnergyRecommendation;
