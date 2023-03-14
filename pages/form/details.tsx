a8e9723a2ddc18b215477dd644d603de0429c4b6
Ashay Parikh <ashayparikh22@gmail.com>
2023-03-14 12:43:10 -0500

    Redesign button loading indicator (#113)

--- pages/form/details.tsx
+++ pages/form/details.tsx
@@ -125,7 +125,7 @@ const Form = () => {
         >
           {didSubmit && <Spinner />}
           Next
-          {didSubmit && <div className="w-8 mx-2" />}
+          {didSubmit && <div className="w-5 mx-4" />}
         </button>
         <Modal show={showModal} setShow={setShowModal} />
       </form>
