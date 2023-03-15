7a0ff84091c0e19955b74685c3e76106aac82923
Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
2023-03-14 20:31:40 -0500

    Add dashboard headers (#117)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -12,7 +12,10 @@ const siteUUID = 'b97f68cd-50e0-49bb-a850-108d4a9f7b7e';
 const Dashboard = () => {
   return (
     <div className="bg-ocf-black max-w-screen-lg w-screen min-h-screen px-4 mb-[75px]">
-      <h1 className="mt-4 text-ocf-gray text-3xl font-bold">Dashboard</h1>
+      <h1 className="mt-4 mb-6 text-ocf-gray text-3xl font-bold">Dashboard</h1>
+      <h2 className="mt-4 mb-2 text-ocf-gray text-base font-semibold">
+        Solar Activity
+      </h2>
       <div className="flex flex-row w-full justify-center space-x-4">
         <SunnyTimeframe siteUUID={siteUUID} />
         <EnergyRecommendation siteUUID={siteUUID} />
@@ -20,6 +23,9 @@ const Dashboard = () => {
       <div className="flex flex-row w-full justify-start">
         <ThresholdGraph siteUUID={siteUUID} />
       </div>
+      <h2 className="mt-4 mb-2 text-ocf-gray text-base font-semibold">
+        Metrics
+      </h2>
       <div className="flex flex-row w-full justify-start">
         <ExpectedTotalOutput siteUUID={siteUUID} />
       </div>
