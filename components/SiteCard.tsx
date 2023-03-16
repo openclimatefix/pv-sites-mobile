ffd257afad35248419ce80bca425936e3e9827f1
Jacky Park <jackypark9852@gmail.com>
2023-03-16 12:50:38 -0600

    Jp/site card (#80)
    * Create test site
    
    * Build sitecard layout
    
    * Change card to a link and span
    
    * Limit card width
    
    * Remove padding in the wrapper
    
    * Add conditional rendering for edit mode
    
    * Add edit icon
    
    * make it look pretty
    
    * decrease navbar height
    
    * implement edit functionality
    
    * change fillRule and clipRule so react doesn't get mad
    
    ---------
    
    Co-authored-by: ericcccliu <liueric360@gmail.com>

---
+++ components/SiteCard.tsx
@@ -0,0 +1,42 @@
+import { FC, PropsWithChildren } from 'react';
+import { DeleteIcon, EditIcon } from './icons';
+import Link from 'next/link';
+
+interface SiteCardProps {
+  isEditMode: boolean;
+}
+
+const SiteCard: FC<PropsWithChildren<SiteCardProps>> = ({
+  isEditMode = false,
+}) => {
+  return (
+    <Link href="/dashboard">
+      <span className="h-fit w-full max-w-lg flex bg-ocf-gray-1000 p-3 rounded-lg font-bold">
+        <div className="flex flex-col flex-1">
+          <h2 className="text-amber text-xl font-semibold">My Home</h2>
+          <div className="flex flex-col mt-2 gap-1">
+            <p className="text-ocf-gray-500 text-xs font-medium">
+              Panel direction: 135
+            </p>
+            <p className="text-ocf-gray-500 font-medium text-xs">
+              Panel tilt: 40
+            </p>
+            <p className="text-ocf-gray-500 font-medium text-xs">
+              Max. capacity: 2800 kWh
+            </p>
+          </div>
+        </div>
+        {isEditMode && (
+          <div className="flex flex-col w-fit">
+            <div className="flex-1">
+              <EditIcon />
+            </div>
+            <DeleteIcon />
+          </div>
+        )}
+      </span>
+    </Link>
+  );
+};
+
+export default SiteCard;
--- components/icons/EditIcon.tsx
+++ components/icons/EditIcon.tsx
@@ -3,8 +3,8 @@ const EditIcon: React.FC = ({}) => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     className="w-6 h-6"
+    stroke="#e4e4e4"
     fill="none"
-    stroke={'currentColor'}
     strokeWidth={1.5}
   >
     <path
