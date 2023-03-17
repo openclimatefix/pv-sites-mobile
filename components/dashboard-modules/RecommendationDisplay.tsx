e0e348672b83e6eb6cc9f176c0c7152c19350bb1
Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
2023-03-16 22:04:17 -0500

    Indicate Nighttime on Energy Recommendation Component (#123)
    * Add nightime functionality to Energy Recommendation component
    
    * Replace hardcoded value
    
    * Rearrange display order and correct isDaytime
    
    * Modularize code

---
+++ components/dashboard-modules/RecommendationDisplay.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import Image from 'next/image';
+
+interface Props {
+  src: string;
+  alt: string;
+  description: string;
+}
+
+const RecommendationDisplay: FC<Props> = ({ src, alt, description }) => {
+  return (
+    <div
+      className="
+          flex-1
+          flex
+          my-2
+          p-4
+          text-center
+          justify-center
+          align-center
+          bg-ocf-gray-1000
+          rounded-2xl"
+    >
+      <Image src={src} alt={alt} width={34.5} height={28.75} />
+      <div className="text-ocf-gray ml-3 self-center font-normal text-left flex-1 max-w-max">
+        <p className="m-0 text-[10px] mb-1">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendationDisplay;
