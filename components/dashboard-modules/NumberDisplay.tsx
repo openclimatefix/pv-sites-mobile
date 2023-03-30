23ce55ae0f409b5e82b825134f43c8b8dc584a02
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-03-30 13:30:36 -0500

    Change NumberDisplay to conditionally display content differently on larger screens

--- components/dashboard-modules/NumberDisplay.tsx
+++ components/dashboard-modules/NumberDisplay.tsx
@@ -7,8 +7,10 @@ interface Props {
 
 const NumberDisplay: FC<Props> = ({ title, value }) => {
   return (
-    <div className="flex-1 p-4 text-center bg-ocf-gray-1000 rounded-2xl h-[100%]">
-      <div className="mb-2 text-xs text-ocf-gray font-semibold">{title}</div>
+    <div className="flex-1 p-4 text-center md:text-left bg-ocf-gray-1000 rounded-2xl h-[100%]">
+      <div className="mb-2 text-xs md:text-lg text-ocf-gray font-semibold">
+        {title}
+      </div>
       <div className="mb-1 text-2xl text-ocf-yellow font-semibold">{value}</div>
     </div>
   );
