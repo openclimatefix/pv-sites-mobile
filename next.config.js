a29c85f735381f0186ef1902ce32453d75cea979
AndrewLester <alester220@gmail.com>
2023-04-04 20:57:08 -0500

    Disable PWA on development

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
 
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV !== 'development',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 module.exports = withPWA(nextConfig);
