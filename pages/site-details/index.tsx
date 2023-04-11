f9b0d383cf8f11a88d925df857c33b183befc6bf
Jacky Park <jackypark9852@gmail.com>
2023-04-10 21:45:53 -0500

    Remove unused enum

--- pages/site-details/index.tsx
+++ pages/site-details/index.tsx
@@ -2,13 +2,7 @@ import { FC } from 'react';
 import { withSites } from '../../lib/utils';
 import SiteDetails from '~/components/SiteDetails';
 
-enum Page {
-  Details = 'Details',
-  Location = 'Location',
-}
-
 const NewSiteDetails: FC = () => {
-  console.log('NewSiteDetails');
   return <SiteDetails />;
 };
 
