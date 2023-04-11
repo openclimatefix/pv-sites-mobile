87cdad086ff96ce68c0c3c5e24dd64d96283a2b1
ericcccliu <liueric360@gmail.com>
2023-04-10 19:28:10 -0500

    refactor number display

--- components/dashboard-modules/NumberDisplay.tsx
+++ components/dashboard-modules/NumberDisplay.tsx
@@ -9,47 +9,34 @@ interface Props {
 }
 
 const NumberDisplay: FC<Props> = ({ title, value, onClick, isLoading }) => {
-  //isLoading = !isLoading;
-
-  return onClick ? (
-    <button
-      className="cursor-pointer transition-all active:scale-95 flex-1 h-[100%] w-[100%]"
-      onClick={onClick}
-    >
-      <div className="flex-1 p-4 flex flex-col justify-center text-center md:text-left bg-ocf-black-500 rounded-2xl w-full h-full">
-        <div
-          className={`mb-2 text-xs md:text-lg text-ocf-gray font-semibold transition-all md:font-medium md:leading-none ${
-            isLoading ? skeleton : ``
-          }`}
-        >
-          {title}
-        </div>
-        <div
-          className={`text-2xl text-ocf-yellow font-semibold leading-none transition-all md:leading-none ${
-            isLoading ? skeleton : ``
-          }`}
-        >
-          {value}
-        </div>
-      </div>
-    </button>
-  ) : (
-    <div className="flex-1 p-4 flex flex-col justify-center text-center md:text-left bg-ocf-black-500 rounded-2xl h-[100%]">
+  const renderDisplay = () => (
+    <div className="flex-1 p-4 flex flex-col justify-center text-center md:text-left bg-ocf-black-500 rounded-2xl w-full h-full">
       <div
-        className={`mb-2 text-xs md:text-lg text-ocf-gray font-semibold md:font-medium md:leading-none ${
+        className={`mb-2 text-xs md:text-lg text-ocf-gray font-semibold transition-all md:font-medium md:leading-none ${
           isLoading ? skeleton : ``
         }`}
       >
         {title}
       </div>
       <div
-        className={`mb-1 text-2xl text-ocf-yellow font-semibold ${
+        className={`text-2xl text-ocf-yellow font-semibold leading-none transition-all md:leading-none ${
           isLoading ? skeleton : ``
         }`}
       >
         {value}
       </div>
     </div>
+  );
+
+  return onClick ? (
+    <button
+      className="cursor-pointer transition-all active:scale-95 flex-1 h-[100%] w-[100%]"
+      onClick={onClick}
+    >
+      {renderDisplay()}
+    </button>
+  ) : (
+    renderDisplay()
   );
 };
 
