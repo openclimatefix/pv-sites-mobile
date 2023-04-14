3856beb190e94a3b3b692e99d9f9134e72f37f27
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-04-14 13:29:43 -0500

    Create basic popup component

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -1,16 +1,31 @@
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, useState } from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
 
 const ContactButton = () => {
   const { user } = useUser();
-  console.log(user);
   const firstName = user && user.given_name ? user.given_name.toString() : '';
   const lastName = user && user.family_name ? user.family_name.toString() : '';
+  const imageURL = user && user.picture ? user.picture.toString() : '';
+  const [displayPopup, setDisplayPopup] = useState(false);
 
   return (
-    <button className="w-[50px] h-[50px] float-right bg-gray-300 rounded-full">
-      {firstName.substring(0, 1) + lastName.substring(0, 1)}
-    </button>
+    <div className="">
+      <button
+        className="w-[40px] h-[40px] float-right bg-[#909090] rounded-full relative text-white"
+        onClick={() =>
+          displayPopup ? setDisplayPopup(false) : setDisplayPopup(true)
+        }
+      >
+        {firstName.substring(0, 1) + lastName.substring(0, 1)}
+      </button>
+      {displayPopup && (
+        <div className="w-[200px] h-[100px] absolute rounded-lg border-[#909090] border-2 bg-ocf-black-900">
+          <div className="ml-[10px] mt-[10px] w-[40px] h-[40px] bg-[#909090] rounded-full text-white items-center">
+            {firstName.substring(0, 1) + lastName.substring(0, 1)}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
