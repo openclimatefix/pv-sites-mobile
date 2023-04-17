334f16a009ee9b74112156fce2493d2825bef018
AndrewLester <alester220@gmail.com>
2023-04-17 14:57:22 -0500

    Remove unused icon

--- lib/components/icons/index.tsx
+++ lib/components/icons/index.tsx
@@ -1,17 +1,13 @@
 import DeleteIcon from './DeleteIcon';
 import LogoutIcon from './LogoutIcon';
-import LocationIcon from './LocationIcon';
 import LeftChevron from './LeftChevron';
 import CloudyIcon from './CloudyIcon';
 import PartlyCloudyIcon from './PartlyCloudyIcon';
 import SunnyIcon from './SunnyIcon';
 
-/* Used to clean up imports of icons */
-
 export {
   DeleteIcon,
   LogoutIcon,
-  LocationIcon,
   LeftChevron,
   CloudyIcon,
   PartlyCloudyIcon,
