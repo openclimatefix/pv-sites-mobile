c226ea71defdb95ab2b51b12f5adf7be3a0cbc20
neha-vard <neha.vard@gmail.com>
2023-04-24 16:47:18 -0500

    Prettier

--- pages/help.tsx
+++ pages/help.tsx
@@ -8,11 +8,24 @@ const Help = () => {
     <div className="bg-ocf-black w-screen min-h-screen max-w-screen-lg px-4">
       <ReactMarkdown
         components={{
-          h1: ({node, ...props}) => <div className="font-semibold text-2xl text-ocf-yellow" {...props}/>,
-          h2: ({node, ...props}) => <div className="font-semibold text-xl text-ocf-yellow" {...props}/>,
-          p: ({node, ...props}) => <div className="text-xl text-white" {...props}/>,
-          ul: ({node, ...props}) => <ul className="list-disc text-xl text-white" {...props}/>,
-          ol: ({node, ...props}) => <ul className="list-decimal text-xl text-white" {...props}/>,
+          h1: ({ node, ...props }) => (
+            <div
+              className="font-semibold text-2xl text-ocf-yellow"
+              {...props}
+            />
+          ),
+          h2: ({ node, ...props }) => (
+            <div className="font-semibold text-xl text-ocf-yellow" {...props} />
+          ),
+          p: ({ node, ...props }) => (
+            <div className="text-xl text-white" {...props} />
+          ),
+          ul: ({ node, ...props }) => (
+            <ul className="list-disc text-xl text-white" {...props} />
+          ),
+          ol: ({ node, ...props }) => (
+            <ul className="list-decimal text-xl text-white" {...props} />
+          ),
         }}
       >
         {content}
