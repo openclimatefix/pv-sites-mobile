d8f3396a7ee34b0e0b35e9cab8e8255c2922f689
Jacky Park <jackypark9852@gmail.com>
2023-04-23 21:46:51 -0500

    Style site cards

--- components/InverterView.tsx
+++ components/InverterView.tsx
@@ -3,21 +3,46 @@ import { InverterCard } from './InverterCard';
 
 interface InverterViewProps {
   siteUUID: string;
+  isSelectMode: boolean;
 }
 
-const InverterView: FC<InverterViewProps> = ({ siteUUID }) => {
+const InverterView: FC<InverterViewProps> = ({ siteUUID, isSelectMode }) => {
   return (
-    <div className="flex justify-center w-full border-2">
-      {/* <div className="flex flex-col w-full"> */}
-      <div className="flex flex-col w-full max-w-4xl border-2 border-orange-600">
-        <h1 className="mt-4 mb-4 text-ocf-gray text-3xl font-bold">
-          Conencted Inverters
-        </h1>
+    <div className="flex justify-center w-full">
+      <div className="flex flex-col w-full max-w-4xl p-3 pt-0">
+        {isSelectMode ? (
+          <>
+            <h1 className="mt-4 text-ocf-gray text-xl">Select Inverters</h1>
+            <h1 className="mb-4 text-ocf-gray text-md">
+              Select the inverters that correspond to your site.
+            </h1>
+          </>
+        ) : (
+          <h1 className="mt-4 mb-4 text-ocf-gray text-xl">
+            Conencted Inverters
+          </h1>
+        )}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-          <InverterCard siteName="Site 1" brand="Brand 1" isReachable={true} />
-          <InverterCard siteName="Site 1" brand="Brand 1" isReachable={true} />
-          <InverterCard siteName="Site 1" brand="Brand 1" isReachable={true} />
-          <InverterCard siteName="Site 1" brand="Brand 1" isReachable={true} />
+          <InverterCard
+            siteName="101 Address Road"
+            brand="Enphase envoy 5"
+            isReachable={true}
+          />
+          <InverterCard
+            siteName="101 Address Road"
+            brand="Enphase envoy 5"
+            isReachable={true}
+          />
+          <InverterCard
+            siteName="101 Address Road"
+            brand="Enphase envoy 5"
+            isReachable={false}
+          />
+          <InverterCard
+            siteName="101 Address Road"
+            brand="Enphase envoy 5"
+            isReachable={false}
+          />
         </div>
       </div>
     </div>
--- pages/test/inverter.tsx
+++ pages/test/inverter.tsx
@@ -3,7 +3,7 @@ import { withSites } from '~/lib/utils';
 
 const Inverter = () => (
   <div className="w-full h-full">
-    <InverterView siteUUID="abcdefg" />
+    <InverterView siteUUID="abcdefg" isSelectMode={true} />
   </div>
 );
 
