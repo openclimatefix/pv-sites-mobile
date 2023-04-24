239dde0449118d4caabcdc8dfac6d962c768f13f
AndrewLester <alester220@gmail.com>
2023-04-24 13:11:26 -0500

    Fix inverters link page

--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -15,10 +15,10 @@ const Button: FC<PropsWithChildren<Props>> = ({
   onClick,
 }) => {
   const outlinedButton =
-    'h-[54px] w-[250px] text-ocf-yellow border-ocf-yellow border-[2px] rounded-md font-semibold hover:bg-ocf-yellow hover:text-black transition-colors duration-300';
+    'h-[54px] w-[250px] text-ocf-yellow border-ocf-yellow border-[2px] rounded-md font-semibold hover:bg-ocf-yellow hover:text-black transition-all duration-300';
 
   const solidButton =
-    'inline-flex gap-[10px] items-center justify-center md:w-[200px] w-10/12 bg-ocf-yellow text-black disabled:bg-ocf-gray disabled:text-ocf-black-600 transition-all duration-500 shadow h-14 max-w-sm text-center rounded-md md:rounded-lg md:font-semibold font-bold text-[16px]';
+    'inline-flex gap-[10px] items-center justify-center md:w-[200px] w-10/12 bg-ocf-yellow text-black disabled:bg-ocf-gray disabled:text-ocf-black-600 transition-all duration-300 shadow h-14 max-w-sm text-center rounded-md md:rounded-lg md:font-semibold font-bold text-[16px]';
 
   return (
     <button
--- lib/components/form/LinkInverters.tsx
+++ lib/components/form/LinkInverters.tsx
@@ -1,83 +1,67 @@
-import { useState } from 'react';
-import InverterGraphicIcon from '../icons/InverterGraphicIcon';
 import Link from 'next/link';
-import useMediaQuery from '~/lib/hooks/useMediaQuery';
+import { FC, useState } from 'react';
 import Button from '../Button';
+import InverterGraphicIcon from '../icons/InverterGraphicIcon';
 import { ChevronRightIcon } from '@heroicons/react/24/solid';
 
-const LinkInverters = () => {
+const LinkInverters: FC = () => {
   const [showInfo, setShowInfo] = useState(false);
-  const mobile = useMediaQuery('(max-width: 768px)');
 
   const mobileSkipButtonClass =
-    'w-full text-ocf-yellow underline text-[14px] mt-[5px]';
-
-  const mobileInverterLinkClass =
-    'h-[54px] w-[308px] bg-ocf-yellow rounded-md font-semibold mb-[20px]';
-
-  const desktopInverterLinkClass =
-    'h-[54px] w-[308px] text-ocf-yellow border-ocf-yellow border-[2px] rounded-md font-semibold';
+    'flex items-center text-ocf-yellow text-[14px] mt-[5px]';
 
   return (
-    <div
-      className="flex h-[520px] w-[320px] flex-col items-center md:mt-[75px] md:h-[550px] md:w-full"
-      suppressHydrationWarning
-    >
-      <div className="mt-[40px] md:mt-[0px]" suppressHydrationWarning>
+    <div className="flex min-h-[70vh] w-full flex-col px-5 md:mt-[75px]">
+      <div className="mt-[40px] self-center md:mt-[0px]">
         <InverterGraphicIcon />
       </div>
-      <div
-        className="mt-[30px] text-[20px] text-white md:w-[485px] md:text-[24px]"
-        suppressHydrationWarning
-      >
+      <div className="mt-[30px] self-center text-[20px] text-white md:w-[485px] md:text-[24px]">
         Would you like to link your inverter with Enode to provide better
         forecasting?
       </div>
-      {mobile && !showInfo && (
+      {!showInfo && (
         <button
-          suppressHydrationWarning
-          className="mt-[5px] w-full text-right text-[14px] text-ocf-yellow underline"
+          className="mt-[5px] block w-full self-center text-right text-[14px] text-ocf-yellow underline md:hidden"
           onClick={() => setShowInfo(true)}
         >
           What&apos;s this?
         </button>
       )}
-      {(!mobile || showInfo) && (
-        <div className="mt-[20px] w-full text-[14px] text-ocf-gray-300 md:w-[485px]">
-          Linking your inverter with Enode gives us access to your solar output
-          data, providing you with better forecasts and more information
-          available within our app.
-        </div>
-      )}
 
-      {mobile && showInfo && (
+      <div
+        className={`mt-[20px] w-full self-center text-[14px] text-ocf-gray-300 md:block md:w-[485px] ${
+          showInfo ? 'block' : 'hidden'
+        }`}
+      >
+        Linking your inverter with Enode gives us access to your solar output
+        data, providing you with better forecasts and more information available
+        within our app.
+      </div>
+
+      {showInfo && (
         <button
-          suppressHydrationWarning
-          className="mt-[5px] w-full text-right text-[14px] text-ocf-yellow underline"
+          className="mt-[5px] block w-full self-center text-right text-[14px] text-ocf-yellow underline md:hidden"
           onClick={() => setShowInfo(false)}
         >
           Show less
         </button>
       )}
-      <div
-        suppressHydrationWarning
-        className="mt-auto flex flex-col justify-center md:mt-[75px] md:justify-start"
-      >
-        <Link href="https://www.google.com/">
+
+      <Link href="https://www.google.com/" passHref>
+        <a className="mt-auto self-center md:mt-5">
           <Button variant="outlined">Yes, link my inverter</Button>
-        </Link>
-        {mobile && (
-          <button className={mobileSkipButtonClass}>Skip this step</button>
-        )}
-      </div>
-      <div
-        suppressHydrationWarning
-        className="mx-auto mt-auto hidden w-11/12 text-[5px] md:flex md:flex-row md:justify-end"
-      >
-        <Button variant="solid">
-          Skip this step
-          <ChevronRightIcon width={20} height={20} />
-        </Button>
+        </a>
+      </Link>
+
+      <div className="mx-auto mb-3 mt-3 flex justify-end md:mb-8 md:mt-auto md:w-10/12">
+        <button className={mobileSkipButtonClass}>
+          Skip this step{' '}
+          <ChevronRightIcon
+            width="24"
+            height="24"
+            className="hidden md:block"
+          />
+        </button>
       </div>
     </div>
   );
