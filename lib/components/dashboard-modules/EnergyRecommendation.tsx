c0d2764b71981b3b2bb044c3ce54e3d0e897b991
ericcccliu <liueric360@gmail.com>
2023-04-30 11:18:46 -0500

    lowercase recommendation display

--- lib/components/dashboard-modules/EnergyRecommendation.tsx
+++ lib/components/dashboard-modules/EnergyRecommendation.tsx
@@ -76,7 +76,7 @@ const EnergyRecommendation: FC<EnergyRecommendationProps> = ({ sites }) => {
       <RecommendationDisplay
         src={`/appliances/icons/${recommended.icon}`}
         alt={recommended.name}
-        description={`You have enough power to ${recommended.action} your ${recommended.name}`}
+        description={`You have enough power to ${recommended.action} your ${recommended.name.toLowerCase()}`}
       />
     );
   }
