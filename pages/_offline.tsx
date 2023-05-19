746c8ea373c47534f4e224e7e36474fd22890e69
AndrewLester <alester220@gmail.com>
2023-05-18 21:31:59 -0400

    Format

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,11 +1,11 @@
 export default function Offline() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white">
-      <h1 className="text-3xl font-bold mb-6">You are offline</h1>
-      <p className="text-gray-600 text-center mb-8">
+    <div className="flex h-screen flex-col items-center justify-center bg-white">
+      <h1 className="mb-6 text-3xl font-bold">You are offline</h1>
+      <p className="mb-8 text-center text-gray-600">
         Please check your internet connection and try again.
       </p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
         Try Again
       </button>
     </div>
