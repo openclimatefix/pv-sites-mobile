fda163cf08cf7d8df65e96904359475a2f929ec9
AndrewLester <alester220@gmail.com>
2023-06-02 10:12:01 -0400

    Remove credentials include

--- lib/swr.ts
+++ lib/swr.ts
@@ -23,7 +23,6 @@ export async function getAuthenticatedRequestOptions(url: string) {
     }
 
     options = {
-      credentials: 'include',
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
